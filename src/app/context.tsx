114b45f5cde1e97aca932d51e496b5dfda050452
Nathan Ranchin <nathan.ranchin@epfl.ch>
2024-10-12 13:56:24 +0200

    context + base page

---
+++ src/app/context.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import "@mysten/dapp-kit/dist/index.css";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+
+const { networkConfig } = createNetworkConfig({
+	testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+
+const queryClient = new QueryClient();
+
+export default function Context({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork={"testnet"}>
+        <WalletProvider autoConnect={true}>
+          {children}
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
+}
